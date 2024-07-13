cea6d8e4e7ba0b7294a19ba7c73793baf19d2e17
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-07-13 11:17:09 +0530

    fix

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.SMTP_PORT || 3000;
 
 app.use(express.json());
-
-const smtpRoute = require('./api/smtp');
-app.use('/api/smtp', smtpRoute);
-
 app.use(express.static(path.join(__dirname)));
+
+const smtpRouter = require("./api/smtp");
+app.use("/api/smtp", smtpRouter);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
