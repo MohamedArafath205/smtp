bf045b307886818178c90f34ab4bce128a28827e
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-07-13 14:43:10 +0530

    fix

--- api/send-email.js
+++ api/send-email.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
+    allowedHeaders: ["*"],
 }));
 
 app.use(bodyParser.json());
